81c975a5cecbb6e07c374382a55d837712fb3cbd
Alenaps <131380975+Alenaps@users.noreply.github.com>
2026-07-27 21:25:42 +0700

    fix https dan urutan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,10 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call([
+        UnitSeeder::class,
         UserSeeder::class,
         PublikasiSeeder::class,
+        JenisNarkobaSeeder::class,
         ]);
     }
 }
